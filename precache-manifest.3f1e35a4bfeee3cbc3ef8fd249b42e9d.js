629cb83b22005e83a5ebbcd74e72ccb7d42b46b6
gorgeous4 <33854597+gorgeous4@users.noreply.github.com>
2019-10-25 14:29:17 +0900

    Updates

---
+++ precache-manifest.3f1e35a4bfeee3cbc3ef8fd249b42e9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ce662aca5553e2021b6d4d21a6002b",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "dce39eb975c689d58f6e",
+    "url": "/movie_app_2019/static/css/main.044a078d.chunk.css"
+  },
+  {
+    "revision": "2bed164abecb2001a4f8",
+    "url": "/movie_app_2019/static/js/2.14ba9984.chunk.js"
+  },
+  {
+    "revision": "dce39eb975c689d58f6e",
+    "url": "/movie_app_2019/static/js/main.8955940d.chunk.js"
+  },
+  {
+    "revision": "44901d82ce6105999ea4",
+    "url": "/movie_app_2019/static/js/runtime-main.0f07e758.js"
+  }
+]);
